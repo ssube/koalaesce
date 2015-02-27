143bb9cd60fab522de41393e8b04113e3043995d
ssube <ssube@redbrickhealth.com>
2015-02-27 12:28:39 -0600

    Build AMD, CommonJS, and UMD modules.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,34 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
 var mocha = require("gulp-mocha");
+var rename = require("gulp-rename");
 
-gulp.task("scripts", function () {
+function compileModules(type, append) {
+    if (append === undefined) append = true;
+
     return gulp.src("src/**/*.js")
-        .pipe(babel())
+        .pipe(babel({
+            modules: type
+        }))
+        .pipe(rename(function (path) {
+            if (append) path.basename += "-" + type;
+        }))
         .pipe(gulp.dest("dist"));
+}
+
+gulp.task("scripts:amd", function () {
+    return compileModules("amd");
 });
+
+gulp.task("scripts:common", function () {
+    return compileModules("common", false);
+});
+
+gulp.task("scripts:umd", function () {
+    return compileModules("umd");
+});
+
+gulp.task("scripts", ["scripts:amd", "scripts:common", "scripts:umd"]);
 
 gulp.task("tests", ["scripts"], function () {
     return gulp.src("tests/**/*.js")
