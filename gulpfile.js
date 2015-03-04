e207f1c682d61fc6d7218f23570428fd013c9f98
ssube <ssube@redbrickhealth.com>
2015-03-04 10:34:22 -0600

    Added lint task and cleaned up a few errors.

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,30 @@ var gulp = require("gulp");
 var babel = require("gulp-babel");
 var mocha = require("gulp-mocha");
 var rename = require("gulp-rename");
+var jshint = require("gulp-jshint");
 
 function compileModules(type, append) {
-    if (append === undefined) append = true;
+    if (append === undefined) {
+        append = true;
+    }
 
     return gulp.src("src/**/*.js")
         .pipe(babel({
             modules: type
         }))
         .pipe(rename(function (path) {
-            if (append) path.basename += "-" + type;
+            if (append) {
+                path.basename += "-" + type;
+            }
         }))
         .pipe(gulp.dest("dist"));
 }
+
+gulp.task("lint", function () {
+    return gulp.src("src/**/*.js")
+        .pipe(jshint())
+        .pipe(jshint.reporter("fail"));
+});
 
 gulp.task("scripts:amd", function () {
     return compileModules("amd");
@@ -28,13 +39,13 @@ gulp.task("scripts:umd", function () {
     return compileModules("umd");
 });
 
-gulp.task("scripts", ["scripts:amd", "scripts:common", "scripts:umd"]);
+gulp.task("scripts", ["lint", "scripts:amd", "scripts:common", "scripts:umd"]);
 
 gulp.task("tests", ["scripts"], function () {
     require("babel/register");
 
     return gulp.src("tests/**/*.js")
-        .pipe(mocha())
+        .pipe(mocha());
 });
 
 gulp.task("default", ["scripts", "tests"]);
